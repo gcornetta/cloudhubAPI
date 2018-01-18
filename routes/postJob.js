e5e625ad409687dd36455bce6bac0a3cf8ba80b6
Javi <fjmateos92@gmail.com>
2018-01-18 12:03:02 +0100

    Flyn test consul

--- routes/postJob.js
+++ routes/postJob.js
@@ -78,7 +78,7 @@ function postJob (req, res) {
 }
 
 console.log(process.env.CONSUL_ADDR);
-request.get(process.env.CONSUL_ADDR, function(err, res, body) {
+request.get(process.env.CONSUL_ADDR+'/v1/health/state/critical', function(err, res, body) {
         if (err){
             console.log(err);
         }else{
@@ -95,6 +95,7 @@ function checkConsulServers(service, tag, callback){
     }else{
         tag = "?tag="+tag.toLowerCase()
     }
+    console.log(process.env.CONSUL_ADDR +'/v1/catalog/service/'+service.toLowerCase()+tag)
     request.get(process.env.CONSUL_ADDR +'/v1/catalog/service/'+service.toLowerCase()+tag, function(err, res, body) {
         if (err){
             console.log(err);
