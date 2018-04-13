633963a44281cde7a89804d64603250d10ab7888
Javi <fjmateos92@gmail.com>
2018-04-13 11:50:13 +0200

    Added limit to getJobs query

--- routes/getJob.js
+++ routes/getJob.js
@@ -6,7 +6,17 @@ function getJob(req, res){
     checkToken.checkToken(token, function(authorized, payload){
         if (authorized && payload){
             var userId = req.get("newtonUser") || getUserId(req.get("Authentication"));
-            req.db.collection('jobs').find({'userId' : userId}).toArray(function(err, docs) {
+            var options = {
+                limit : 20,
+                skip : 0
+            }
+            if (req.query.limit){
+                options.limit = parseInt(req.query.limit)
+            }
+            if (req.query.offset){
+                options.skip = parseInt(req.query.offset)
+            }
+            req.db.collection('jobs').find({'userId' : userId}, options).toArray(function(err, docs) {
                 if (err){
                     res.status(500);
                     res.json(err);
